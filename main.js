87b12d96ce7b17a4bf2c9a995bf3d6d2eaae985e
Iris Veress <68661896+VeressIris@users.noreply.github.com>
2024-08-13 02:16:55 +0300

    finished 2 player pong

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ https://sprig.hackclub.com/gallery/getting_started
 
 @title: pong
 @author: 
-@tags: []
-@addedOn: 2024-00-00
+@tags: ['beginner']
+@addedOn: 2024-08-13
 */
 
 const player1 = "p";
@@ -13,7 +13,7 @@ const player2 = "e";
 const ball = "b";
 
 setLegend(
-  [ player1, bitmap`
+  [player1, bitmap`
 7777777.........
 7777777.........
 7777777.........
@@ -30,7 +30,7 @@ setLegend(
 7777777.........
 7777777.........
 7777777.........`],
-  [ player2, bitmap`
+  [player2, bitmap`
 .........3333333
 .........3333333
 .........3333333
@@ -46,8 +46,8 @@ setLegend(
 .........3333333
 .........3333333
 .........3333333
-.........3333333` ],
-  [ ball, bitmap`
+.........3333333`],
+  [ball, bitmap`
 ....55555555....
 ...5555555555...
 ..555555555555..
@@ -64,7 +64,7 @@ setLegend(
 ..555555555555..
 ...5555555555...
 ....55555555....`]
-)
+);
 
 const level = map`
 ....................
@@ -82,11 +82,11 @@ p..................e
 ....................
 ....................
 ....................
-....................`
+....................`;
 
 setMap(level);
 
-setSolids([ player1, player2 ]);
+setSolids([player1, player2]);
 
 setPushables({
   [player1]: [],
@@ -95,58 +95,63 @@ setPushables({
 });
 
 var isGameOver = false;
+var gameLoop;
+var directionY = 1;
+var directionX = 1;
+var score = 0;
+var ballObj = getFirst(ball);
 
-// player1 movement
-const p1 = getAll(player1);
+// player 1 movement
+var p1 = getAll(player1);
 onInput("w", () => {
   if (isGameOver) return;
   for (let i = 0; i < p1.length; i++) {
     p1[i].y -= 1;
   }
-})
+});
 onInput("s", () => {
   if (isGameOver) return;
   for (let i = p1.length - 1; i >= 0; i--) {
     p1[i].y += 1;
   }
-})
+});
 
-// player2 movement
-const p2 = getAll(player2);
+// player 2 movement
+var p2 = getAll(player2);
 onInput("i", () => {
   if (isGameOver) return;
   for (let i = 0; i < p2.length; i++) {
     p2[i].y -= 1;
   }
-})
+});
 onInput("k", () => {
   if (isGameOver) return;
   for (let i = p2.length - 1; i >= 0; i--) {
     p2[i].y += 1;
   }
-})
+});
 
-const ballObj = getFirst(ball);
 function checkHitY() {
-  if (ballObj.y == 0 || ballObj.y == 15) return true;
-  return false;
+  return ballObj.y == 0 || ballObj.y == 15;
 }
 
 function gameOver() {
   if (ballObj.x == 0) {
+    clearText();
     addText('Player 2 wins!', {
       x: 3,
       y: 6,
       color: color`3`
-    })
+    });
     return true;
   }
   if (ballObj.x == 19) {
+    clearText();
     addText('Player 1 wins!', {
       x: 3,
       y: 6,
       color: color`7`
-    })
+    });
     return true;
   }
   return false;
@@ -164,24 +169,55 @@ function checkPaddleHit() {
   return false;
 }
 
-var directionY = 1;
-var directionX = 1;
-var gameLoop = setInterval(() => {
-  // move ball
-  ballObj.x += directionX;
-  ballObj.y += directionY;
+function resetGameState() {
+  clearText();
+  setMap(level);
+  p1 = getAll(player1);
+  p2 = getAll(player2);
+  ballObj = getFirst(ball);
+  isGameOver = false;
+  score = 0;
+  directionY = 1;
+  directionX = 1;
+  clearInterval(gameLoop);
+}
 
-  // bounce ball off top walls
-  if (checkHitY()) {
-    directionY *= -1;
-  }
+function startGame() {
+  gameLoop = setInterval(() => {
+    ballObj.x += directionX;
+    ballObj.y += directionY;
 
-  // bounce ball off paddles
-  if (checkPaddleHit()) {
-    directionY *= -1;
-    directionX *= -1;
-  } else if (gameOver()) {
-    isGameOver = true;
-    clearInterval(gameLoop);
-  }
-}, 150)
+    // bounce ball off top and bottom walls
+    if (checkHitY()) {
+      directionY *= -1;
+    }
+
+    // bounce ball off paddles
+    if (checkPaddleHit()) {
+      directionY *= -1;
+      directionX *= -1;
+      score++;
+    } else if (gameOver()) {
+      isGameOver = true;
+      addText('Score: ' + score.toString(), {
+        x: 6,
+        y: 8,
+        color: color`0`
+      });
+      addText(`Press 'i' to \n play again`, {
+        x: 4,
+        y: 10,
+        color: color`0`
+      });
+      clearInterval(gameLoop);
+    }
+  }, 125);
+}
+
+onInput("a", () => {
+  if (!isGameOver) return;
+  resetGameState();
+  startGame();
+});
+
+startGame();
